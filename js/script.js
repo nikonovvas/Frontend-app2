ccc4b14054c17a69c5f74e0dc9d85ba703cf320c
nikonovvas <nikonovvas@mail.ru>
2021-11-25 00:08:47 +0700

    feature-mobile-marrkup(finish)

--- js/script.js
+++ js/script.js
@@ -2,10 +2,19 @@ $(document).ready(function() {
     $('.header__burger').click(function(){
         $('.header__burger, .header__menu').toggleClass('active');
 	$('body').toggleClass('lock');
+    });
+
+    $('.header__menu').click(function(){
+       $('.header__burger, .header__menu').removeClass('active');
+    $('body').removeClass('lock');
+    });
 });
 
-$('.header__menu').click(function(){
-       $('.header__burger, .header__menu').removeClass('active');
-       $('body').removeClass('lock');
-});
-});
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1){
+    $('header').addClass("scroll");
+    }
+    else{
+    $('header').removeClass("scroll");
+    }
+    });
