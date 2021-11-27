6c2d65e50d5892870cd8c2d48228c13c048eca71
nikonovvas <nikonovvas@mail.ru>
2021-11-27 09:06:10 +0700

    feature-mobile-markup
    Внесены исправления

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
-$(document).ready(function() {
-    $('.header__burger').click(function(){
-        $('.header__burger, .header__menu').toggleClass('active');
-	$('body').toggleClass('lock');
-    });
+function onClickMenu(){
+    document.getElementById('btn-menu').classList.toggle('active');
+    document.getElementById('head-menu').classList.toggle('active');
+};
 
-    $('.header__menu').click(function(){
-       $('.header__burger, .header__menu').removeClass('active');
-    $('body').removeClass('lock');
-    });
+window.onscroll = function (){
+    if(window.pageYOffset >= 100){
+        document.getElementById('header').classList.add('scroll');
+    }
+    else if (window.pageYOffset < 100) {
+        document.getElementById('header').classList.remove('scroll');
+    }
+}
+
+window.addEventListener('scroll', () => {
+	let scrollDistance = window.scrollY;
+		document.querySelectorAll('.section').forEach((el, i) => {
+			if (el.offsetTop - document.querySelector('.nav').clientHeight <= scrollDistance) {
+				document.querySelectorAll('.nav a').forEach((el) => {
+					if (el.classList.contains('active')) {
+						el.classList.remove('active');
+					}
+				});
+				document.querySelectorAll('.nav li')[i].querySelector('a').classList.add('active');
+			}
+		});
 });
 
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1){
-    $('header').addClass("scroll");
-    }
-    else{
-    $('header').removeClass("scroll");
-    }
-    });
+function readMore(){
+	var more=document.getElementById('show-more');
+	var more__btn=document.getElementById('more__btn');
+
+	if(more.style.display == 'inline'){
+		more.style.display = 'none';
+		more__btn.innerHTML = 'Больше туров';
+	}
+	else{
+		more.style.display = 'inline';
+		more__btn.innerHTML = 'Cкрыть';
+	}
+}
